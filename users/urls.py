46c5729646d2b80d52cb9de7d47c5a33962c383b
ChangZero <kimcy9973@gmail.com>
2022-11-06 13:50:52 +0900

    로그아웃 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('', auth_view.LoginView.as_view(
         template_name='users/login.html'), name='users-login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'),
+         name='users-logout'),
+
 ]
