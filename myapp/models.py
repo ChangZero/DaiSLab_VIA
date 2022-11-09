14936c8600465d9e90890b1f39a1472691e6da37
ChangZero <kimcy9973@gmail.com>
2022-11-09 15:20:41 +0900

    모델 수정

--- myapp/models.py
+++ myapp/models.py
@@ -11,34 +11,18 @@ from django.contrib.auth.models import User
 class PostModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(null=True, blank=True)
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
 
     image = models.ImageField(
-        upload_to='images/', validators=[FileExtensionValidator(['png', 'jpg'])], null=True, blank=True)
+        upload_to='images/', validators=[FileExtensionValidator(['png', 'jpg'])], null=True)
 
-    metadata = models.FileField(upload_to='files/', blank=True, null=True)
+    metadata = models.FileField(
+        upload_to='files/', validators=[FileExtensionValidator(['csv', 'json'])], null=True)
 
     class Meta:
         ordering = ('-date_created',)
 
     def __str__(self):  # PostModel의 title을 admin에 넘겨주기 위해서 만듬
         return self.title
-
-
-# class Photo(models.Model):
-#     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, null=True)
-#     image = models.ImageField(
-#         upload_to='images/', validators=[FileExtensionValidator(['png', 'jpg'])], null=True, blank=True)
-
-#     def __str__(self):
-#         return self.image
-
-
-# class File(models.Model):
-#     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, null=True)
-#     metadata = models.FileField(upload_to='files/', blank=True, null=True)
-
-    # def __str__(self):
-    #     return self.metadata
